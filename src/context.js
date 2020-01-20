bcd08a4339768be541145f3b602010ff39c4bce6
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2020-01-20 17:31:04 +0100

    Form submit action to context

--- src/context.js
+++ src/context.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
     switch(action.type) {
         case "DELETE_CONTACT":
-            return{
+            return {
                 ...state,
                 contacts: state.contacts.filter(
                     contact => contact.id !== action.payload)
-            }
+            };
+            case "ADD_CONTACT":
+                return {
+                    ...state,
+                    contacts: [action.payload, ...state.contacts]
+                };
         default:
             return state;
     }
-}
+};
 
 export class Provider extends Component {
     state = {
