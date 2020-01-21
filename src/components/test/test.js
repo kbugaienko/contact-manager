9fd19c7cbdf16c2e31bb2d00fefb0eb8ccb175b6
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2020-01-21 16:05:22 +0100

    Use get request and install axios
    * install via `npm i axios`

--- src/components/test/test.js
+++ src/components/test/test.js
@@ -2,35 +2,43 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body: ''
   };
 
   componentDidMount() {
-    console.log('componentDidMount....');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({
+        title: data.title,
+        body: data.body
+      }));
   }
 
-  UNSAFE_componentWillMount() {
-    console.log('componenWillMount....');
-  }
+  // UNSAFE_componentWillMount() {
+  //   console.log('componenWillMount....');
+  // }
 
-  UNSAFE_componentDidUpdate() {
-    console.log('componentDidUpdate.....');
-  }
+  // UNSAFE_componentDidUpdate() {
+  //   console.log('componentDidUpdate.....');
+  // }
 
-  UNSAFE_componentWillReceiveProps(nextProps, nextState) {
-    console.log('componentWillReceiveProps.....');
-  }
+  // UNSAFE_componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('componentWillReceiveProps.....');
+  // }
   
-  static getDerivedStateFromProps(nextProps, prevProps) {
-    return {
-      test: 'something'
-    };
-  }
+  // static getDerivedStateFromProps(nextProps, prevProps) {
+  //   return {
+  //     test: 'something'
+  //   };
+  // }
 
   render() {
+    const { title, body } = this.state;
     return(
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
--- src/context.js
+++ src/context.js
@@ -1,59 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case "DELETE_CONTACT":
-            return {
-                ...state,
-                contacts: state.contacts.filter(
-                    contact => contact.id !== action.payload)
-            };
-            case "ADD_CONTACT":
-                return {
-                    ...state,
-                    contacts: [action.payload, ...state.contacts]
-                };
-        default:
-            return state;
-    }
+  switch(action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload)
+      };
+      case "ADD_CONTACT":
+        return {
+          ...state,
+          contacts: [action.payload, ...state.contacts]
+        };
+    default:
+      return state;
+  }
 };
 
 export class Provider extends Component {
-    state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'John Doe',
-                email: 'jdoe@gmail.com',
-                phone: '555-555-555'
-            },
-            {
-                id: 2,
-                name: 'Karen Smith',
-                email: 'kareoe@gmail.com',
-                phone: '222-435-555'
-            },
-            {
-                id: 3,
-                name: 'Henry Buuty',
-                email: 'herrtu@gmail.com',
-                phone: '888-567-345'
-            }
-        ],
-        dispatch: action => {
-            this.setState(state => reducer(state, action))
-        }
+  state = {
+    contacts: [],
+    dispatch: action => {
+      this.setState(state => reducer(state, action))
     }
+  }
+  
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => this.setState({contacts: res.data}));
+  }
 
-    render() {
-        return(
-            <Context.Provider value={this.state}>
-                {this.props.children}
-            </Context.Provider>
-        );
-    }
+  render() {
+    return(
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
 
 export const Consumer = Context.Consumer;
