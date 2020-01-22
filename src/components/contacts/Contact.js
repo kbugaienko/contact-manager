24f757729f57a0d0ed1791b3bbc277c199bef524
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2020-01-22 15:53:36 +0100

    Edit contact component and fetch contact

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { Consumer } from '../../context';
 
 import './contact.css';
@@ -48,16 +49,31 @@ class Contact extends Component {
           return(
             <div className="card card-body mb-3">
               <h4>{name} {' '}
-                <i onClick={this.onShowClick}
-                className="fas fa-sort-down"
-                style={{ cursor: 'pointer'}}></i>
+
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer'}}
+                />
+
                 <i className="fas fa-times" style={{
                   cursor: 'pointer',
                   float: 'right',
-                  color: 'red' }} onClick={this.onDeleteClick.bind(
-                    this, id, dispatch)
-                  }>
-                </i>
+                  color: 'red' }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+
+                <Link to={`contact/edit/${id}`}>
+                  <i 
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'black',
+                      marginRight:'1rem'
+                    }}  
+                  />
+                </Link>
               </h4>
               {showContactInfo ? (<ul className="list-group">
                 <li className="list-group-item">{email}</li>
