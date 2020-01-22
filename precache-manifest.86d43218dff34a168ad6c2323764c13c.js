6492ccad611c78407f864766fdc640f63b4d553b
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2020-01-22 16:49:30 +0100

    Updates

---
+++ precache-manifest.86d43218dff34a168ad6c2323764c13c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568073c034d349df045c95b938c23efc",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "c582223f4ca8ffcf8cec",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6225899dcf27012c27a0",
+    "url": "/contact-manager/static/css/main.6c3cab30.chunk.css"
+  },
+  {
+    "revision": "c582223f4ca8ffcf8cec",
+    "url": "/contact-manager/static/js/2.e4a8b3ad.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contact-manager/static/js/2.e4a8b3ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6225899dcf27012c27a0",
+    "url": "/contact-manager/static/js/main.5ab44420.chunk.js"
+  },
+  {
+    "revision": "7fc69bf43d6d11c29c63",
+    "url": "/contact-manager/static/js/runtime-main.eeb47f97.js"
+  }
+]);
